a1e0a2520b4d9e8f8987336e43fcf9ed205983b5
alianhakim9 <alianhakim9@gmail.com>
2023-12-21 10:43:25 +0700

    POST Request

--- learning-progress/learn-networking/LearnNetworking.tsx
+++ learning-progress/learn-networking/LearnNetworking.tsx
@@ -5,6 +5,8 @@ import {
   StyleSheet,
   FlatList,
   ActivityIndicator,
+  TextInput,
+  Button,
 } from 'react-native';
 
 type Post = {
@@ -18,6 +20,9 @@ export default function LearnNetworking() {
   const [postList, setPostList] = useState<Post[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const [isPosting, setIsPosting] = useState(false);
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(
@@ -35,6 +40,26 @@ export default function LearnNetworking() {
     setRefreshing(false);
   };
 
+  const addPost = async () => {
+    setIsPosting(true);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody,
+      }),
+    });
+
+    const newPost = await response.json();
+    setPostList([newPost, ...postList]);
+    setPostTitle('');
+    setPostBody('');
+    setIsPosting(false);
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -49,36 +74,57 @@ export default function LearnNetworking() {
   }
 
   return (
-    <View style={styles.listContainer}>
-      <FlatList
-        data={postList}
-        renderItem={({item}) => (
-          <View key={item.id} style={styles.card}>
-            <Text style={styles.titleText}>{item.title}</Text>
-            <Text style={styles.bodyText}>{item.body}</Text>
-          </View>
-        )}
-        ItemSeparatorComponent={() => (
-          <View
-            style={{
-              height: 8,
-            }}
-          />
-        )}
-        ListHeaderComponent={() => (
-          <Text style={styles.headerText}>Post List</Text>
-        )}
-        ListFooterComponent={() => (
-          <Text style={styles.footerText}>End Of List</Text>
-        )}
-        ListEmptyComponent={() => (
-          <Text style={styles.emptyText}>No Posts Found</Text>
-        )}
-        showsVerticalScrollIndicator={false}
-        refreshing={refreshing}
-        onRefresh={handleRefresh}
-      />
-    </View>
+    <>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={postList}
+          renderItem={({item}) => (
+            <View key={item.id} style={styles.card}>
+              <Text style={styles.titleText}>{item.title}</Text>
+              <Text style={styles.bodyText}>{item.body}</Text>
+            </View>
+          )}
+          ItemSeparatorComponent={() => (
+            <View
+              style={{
+                height: 8,
+              }}
+            />
+          )}
+          ListHeaderComponent={() => (
+            <Text style={styles.headerText}>Post List</Text>
+          )}
+          ListFooterComponent={() => (
+            <Text style={styles.footerText}>End Of List</Text>
+          )}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyText}>No Posts Found</Text>
+          )}
+          showsVerticalScrollIndicator={false}
+          refreshing={refreshing}
+          onRefresh={handleRefresh}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Post Title"
+          value={postTitle}
+          onChangeText={setPostTitle}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Post Body"
+          value={postBody}
+          onChangeText={setPostBody}
+        />
+        <Button
+          title={isPosting ? 'Adding...' : 'Add Post'}
+          onPress={addPost}
+          disabled={isPosting}
+        />
+      </View>
+    </>
   );
 }
 
@@ -122,5 +168,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  inputContainer: {
+    backgroundColor: '',
+    padding: 18,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
   },
 });
