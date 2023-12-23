99180301b22fe24a3bcbac462f90aede60436f4e
alianhakim9 <alianhakim9@gmail.com>
2023-12-23 13:17:24 +0700

    Stack Navigation Options

--- learning-progress/learn-navigation/NavigationApp.tsx
+++ learning-progress/learn-navigation/NavigationApp.tsx
@@ -3,14 +3,46 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from './RootStackParamList';
 import AboutScreen from './screens/AboutScreen';
 import HomeScreen from './screens/HomeScreen';
+import {Alert, Pressable, StatusBar, Text} from 'react-native';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function NavigationApp() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#5D3587',
+          },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerRight: () => (
+            <Pressable onPress={() => Alert.alert('Menu button pressed!')}>
+              <Text
+                style={{
+                  fontSize: 16,
+                  color: '#FFD1E3',
+                }}>
+                Menu
+              </Text>
+            </Pressable>
+          ),
+          contentStyle: {
+            backgroundColor: '#A367B1',
+          },
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Welcome Home',
+            fullScreenGestureEnabled: true,
+          }}
+        />
         <Stack.Screen
           name="About"
           component={AboutScreen}
@@ -19,6 +51,7 @@ export default function NavigationApp() {
           }}
         />
       </Stack.Navigator>
+      <StatusBar backgroundColor="#392467" />
     </NavigationContainer>
   );
 }
